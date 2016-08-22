b9529778652a84ef99745e7810fe19f02321a957
alsabater <alvaro@sabater.eu>
2016-08-23 00:14:54 +0200

    Users and Admins created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,12 +43,4 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "alvaro@sabater.eu",
-    password: "961550732"
-  }
 end
